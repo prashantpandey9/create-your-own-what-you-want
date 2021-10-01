d18d7e4b200e808d3006d80639568e6bf8160526
Sunena-123 <91733745+Sunena-123@users.noreply.github.com>
2021-10-01 19:31:01 +0530

    Update finding prime or not.py

--- finding prime or not.py
+++ finding prime or not.py
@@ -1,13 +1,22 @@
-def factor(n):
-         l=[]
-         for i in range(1,n+1):
-                  if n%i==0:
-                           w=l.append(i)
-         print "Factor of this no. are ",l
-         if len(l)==2:
-                  print "It is a Prime no.!!"
-         else:
-                  print "It Is  Not A prime No."
-n=int(raw_input("Enter a Prime No."))
-factor(n)
+
+# Python program to check if
+# given number is prime or not
  
+num = 11
+ 
+# If given number is greater than 1
+if num > 1:
+ 
+    # Iterate from 2 to n / 2
+    for i in range(2, int(num/2)+1):
+ 
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+ 
+else:
+    print(num, "is not a prime number")
