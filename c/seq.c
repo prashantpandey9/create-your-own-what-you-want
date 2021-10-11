15d8c5800ce188feee4bec8126df7c28a26fcc53
kakurasan <kakurasan@users.noreply.github.com>
2021-10-11 22:48:10 +0900

    c/seq.c: Add a function that takes first/last values

--- c/seq.c
+++ c/seq.c
@@ -3,15 +3,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void seq(int last) {
+void seq_from_last(int last) {
     int i;
     for (i = 1; i <= last; i++) {
         printf("%d\n", i);
     }
 }
 
+void seq_from_first_and_last(int first, int last) {
+    int i;
+    for (i = first; i <= last; i++) {
+        printf("%d\n", i);
+    }
+}
+
 int main() {
-    seq(5);
+    seq_from_last(5);               /* 1 2 3 4 5 */
+    seq_from_first_and_last(7, 9);  /* 7 8 9 */
 
     return EXIT_SUCCESS;
 }
