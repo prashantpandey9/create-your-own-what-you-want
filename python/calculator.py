f8595f88a27200b2ec719a74df001d273095cde1
Ashish Kumar Mishra <78248287+Ash1912@users.noreply.github.com>
2022-10-05 19:54:24 +0530

    Add files via upload

---
+++ python/calculator.py
@@ -0,0 +1,16 @@
+n1= input("Enter a no.: ")
+n2= input("Enter a no.: ")
+x= int(n1)
+y= int(n2)
+a1= input("Enter a choice :")
+a= int(a1)
+if a==1:
+ print(x+y)
+elif a==2:
+ print(x-y)
+elif a==3:
+ print(x*y)
+elif a==4:
+ print(x/y)
+elif a==5:
+ print(x%y)
